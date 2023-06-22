07e882ccbb07cfc4701c39afaddf684866a327b0
Richard2203 <55164319+Richard2203@users.noreply.github.com>
2023-06-22 01:55:29 -0600

    API base casi terminado, falta update

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -15,8 +15,42 @@ const getUsers = async (req, res) => {
 	res.status(200).json(response.rows);
 };
 
-const createUsers = async (req, res) => {};
+const getMessageUsuarioById = async (req, res) => {
+	const response = await pool.query(
+		'SELECT * FROM usuario WHERE idcliente = $1',
+		[req.params.id]
+	);
+	res.json(response.rows);
+};
+
+const createUsers = async (req, res) => {
+	const { nombre, email, asunto, mensaje } = req.body;
+	const response = await pool.query(
+		'INSERT INTO usuario(nombre, email, asunto, mensaje) values($1,$2,$3,$4)',
+		[nombre, email, asunto, mensaje]
+	);
+
+	console.log(response);
+	res.json({
+		message: 'mensaje de formulario guardado',
+		body: {
+			user: { nombre },
+		},
+	});
+};
+
+const deleteMessageUserById = async (req, res) => {
+	const id = req.params.id;
+	const response = await pool.query(
+		'DELETE FROM usuario WHERE idcliente=$1',
+		[id]
+	);
+	res.json('Mensaje de usuario ' + id + ' eleminado');
+};
 
 module.exports = {
 	getUsers,
+	getMessageUsuarioById,
+	createUsers,
+	deleteMessageUserById,
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,16 @@
 const { Router } = require('express');
 const router = Router();
 
-const { getUsers, createUsers } = require('../controllers/index.controller');
+const {
+	getUsers,
+	getMessageUsuarioById,
+	createUsers,
+	deleteMessageUserById,
+} = require('../controllers/index.controller');
 
 router.get('/users', getUsers);
-
+router.get('/users/:id', getMessageUsuarioById);
 router.post('/users', createUsers);
+router.delete('/users/:id', deleteMessageUserById);
 
 module.exports = router;
