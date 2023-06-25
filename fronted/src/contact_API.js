4a7d7f0de65daf54c87d425a37661b6835d9b143
Richard2203 <55164319+Richard2203@users.noreply.github.com>
2023-06-24 23:31:41 -0600

    se termino el envio de formulario

--- fronted/src/contact_API.js
+++ fronted/src/contact_API.js
@@ -1,18 +1,24 @@
 const form = document.getElementById('form_contacto');
 
-form.addEventListener('submit', function (e) {
+form.addEventListener('submit', async function (e) {
 	e.preventDefault();
 
 	const prePayload = new FormData(form);
 	const payload = new URLSearchParams(prePayload);
 
-	console.log([...payload]);
-
-	fetch('http://localhost:3000/users', {
+	const response = await fetch('http://localhost:3000/users', {
 		method: 'POST',
 		body: payload,
 	})
 		.then((res) => res.json())
 		.then((data) => console.log(data))
 		.then((err) => console.log(err));
+
+	Swal.fire(
+		document.getElementById('name').value,
+		'Tu mensaje se envio correctamente',
+		'success'
+	);
+
+	form.reset();
 });
